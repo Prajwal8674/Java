2c68f79526981846651e4bb89c364750eb09b21e
TLS-Escanor <patilprajwal8804@gmail.com>
2023-10-06 23:55:18 +0530

    commit

---
+++ Hashcode/src/AppendElement/Append.java
@@ -0,0 +1,20 @@
+package AppendElement;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Append {
+    public static void main(String[] args)
+    {
+        HashSet obj=new HashSet<>();
+        obj.add("Red");
+        obj.add("Green");
+        obj.add("Black");
+        obj.add("White");
+        obj.add("Pink");
+        obj.add("Yellow");
+        System.out.println("The linked list : " + obj);
+
+    }
+
+}
