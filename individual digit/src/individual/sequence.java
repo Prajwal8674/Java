5da5717dd4c7f5451694666b33e3c1dde7c62936
TLS-Escanor <patilprajwal8804@gmail.com>
2024-01-02 16:25:01 +0530

    commit

---
+++ individual digit/src/individual/sequence.java
@@ -0,0 +1,26 @@
+package individual;
+import java.util.Scanner;
+
+public class sequence {
+    public static void main (String[] args)
+    {
+int[] num= new int[6];
+int a;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter 6 non negative digits");
+        for(int i=0;i<1;i++)
+        {
+            num[i]=sc.nextInt();
+        }
+
+       for(int i=0;i<1;i++)
+       {
+           System.out.println(i);
+           for( i=0;i<1;i++)
+           {
+               System.out.println(" ");
+           }
+       }
+
+    }
+}
