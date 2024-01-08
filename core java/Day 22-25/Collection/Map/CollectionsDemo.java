c19db0752976d471dd2453ccdbea242b29fbc189
intellij <patilprajwal8804@gmail.com>
2024-01-09 00:37:50 +0530

    commit

---
+++ core java/Day 22-25/Collection/Map/CollectionsDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class CollectionsDemo {
+
+   public static void main(String[] args) {
+      Map m1 = new HashMap(); 
+      m1.put("Zara", "8");
+      m1.put("Mahnaz", "31");
+      m1.put("Ayan", "12");
+      m1.put("Daisy", "14");
+	  //m1.put("Daisy", "13");
+      System.out.println();
+      System.out.println(" Map Elements");
+      System.out.print("\t" + m1);
+   }
+}
