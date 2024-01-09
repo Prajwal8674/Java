124affd9e7f92ed3c968787510e3e6b5a125c7ff
intellij <patilprajwal8804@gmail.com>
2024-01-10 01:19:58 +0530

    commit

---
+++ core java/Day 6/Object and Classes/Student1.java
@@ -0,0 +1,10 @@
+class Student1{  
+ int id=10;//data member (also instance variable)  
+ String name="name";//data member(also instance variable)  
+  
+ public static void main(String args[]){  
+  Student1 s1=new Student1();//creating an object of Student  
+  System.out.println(s1.id);  
+  System.out.println(s1.name);  
+ }  
+}  
