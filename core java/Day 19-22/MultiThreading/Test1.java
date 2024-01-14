bf3954f26c0d197e510c8cf3d5e20e8fcd0f608f
intellij <patilprajwal8804@gmail.com>
2024-01-15 01:20:06 +0530

    commit

---
+++ core java/Day 19-22/MultiThreading/Test1.java
@@ -0,0 +1,9 @@
+class Test1{
+public static void main(String args[]) throws InterruptedException{
+System.out.println("Thread1");
+Thread.sleep(2000);
+System.out.println("Thread2");
+Thread.sleep(2000);
+
+}
+}
