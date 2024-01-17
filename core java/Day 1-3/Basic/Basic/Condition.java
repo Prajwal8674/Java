77c0dafa5a1af35bc143e984e45d8dc40e61320c
intellij <patilprajwal8804@gmail.com>
2024-01-18 00:38:34 +0530

    commit

---
+++ core java/Day 1-3/Basic/Basic/Condition.java
@@ -0,0 +1,12 @@
+class Condition {
+  public static void main(String[] args) {
+    boolean learning = false;
+ 
+    if (learning) {
+      System.out.println("Java programmer");
+    }
+    else {
+      System.out.println("You are not java programmer");
+    }
+  }
+}
