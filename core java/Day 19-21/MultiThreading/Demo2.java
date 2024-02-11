0b92ee90b778eea60df573814e556c324154203e
intellij <patilprajwal8804@gmail.com>
2024-02-12 00:47:07 +0530

    commit

---
+++ core java/Day 19-21/MultiThreading/Demo2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class MyThread extends Thread{
+	boolean stop = false;
+	public void run(){
+		for(int i=0;i<=10000;i++){
+			System.out.println(i);
+			if(stop) return;
+				
+		}
+	}
+}
+class Demo2{
+	public static void main(String args[])throws IOException{
+		MyThread obj = new MyThread();
+		Thread t = new Thread(obj);
+		t.start();
+		System.in.read();
+		obj.stop = true;
+	}
+}
