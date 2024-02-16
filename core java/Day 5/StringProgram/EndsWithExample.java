5bee5b6487e40edac2db30a3f03dc61ae03ff3de
intellij <patilprajwal8804@gmail.com>
2024-02-17 00:48:39 +0530

    commit

---
+++ core java/Day 5/StringProgram/EndsWithExample.java
@@ -0,0 +1,6 @@
+public class EndsWithExample{  
+public static void main(String args[]){  
+String s1="java programming language";  
+System.out.println(s1.endsWith("language"));  
+System.out.println(s1.startsWith("Hello"));  
+}} 
