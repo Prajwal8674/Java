a7dbd0b85ebae232d5cc7eeaa9b3a45493ebc127
intellij <patilprajwal8804@gmail.com>
2024-02-22 01:19:19 +0530

    commit

---
+++ core java/Day 16/Access Modifiers/Test.java
@@ -0,0 +1,9 @@
+package neo.solution.m; 
+//import neo.solution;  
+class Test{  
+public static void main(String[] args){  
+Student s=new Student();  
+s.setName("Manish");  
+System.out.println(s.getName());  
+}  
+}  
