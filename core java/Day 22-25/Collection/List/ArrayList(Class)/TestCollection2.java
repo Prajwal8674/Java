c3dd99d0817bb32cc59c1c3a44ac6f9f74bedf3c
intellij <patilprajwal8804@gmail.com>
2024-03-12 01:24:46 +0530

    commit

---
+++ core java/Day 22-25/Collection/List/ArrayList(Class)/TestCollection2.java
@@ -0,0 +1,12 @@
+import java.util.*;  
+class TestCollection2{  
+ public static void main(String args[]){  
+  ArrayList<String> al=new ArrayList<String>();  
+  al.add("Manish");  
+  al.add("Srau");  
+  al.add("Manish");  
+  al.add("Ajay");  
+  for(String obj:al)  
+    System.out.println(obj);  
+ }  
+} 
