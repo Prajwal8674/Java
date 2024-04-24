9fb8c155a4fc1252f4c8514b04c74132fbcd87ff
intellij <patilprajwal8804@gmail.com>
2024-04-25 00:54:53 +0530

    commit

---
+++ core java/Day 19-21/MultiThreading/HashSetdemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HashSetdemo
+{
+  public static void main(String args[])
+  {
+  boolean[] ba = new boolean[7];
+  HashSet hs = new new HashSet();
+  
+  ba[0] = hs.add("a");
+  for(int i=0;i<ba.length;i++)
+  system.out.println(""+ba[i]);
+  }
+}
