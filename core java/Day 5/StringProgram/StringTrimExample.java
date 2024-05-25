82d463ab0bb38c679c1bb4954f7b650067b942af
intellij <patilprajwal8804@gmail.com>
2024-05-26 01:14:58 +0530

    commit

---
+++ core java/Day 5/StringProgram/StringTrimExample.java
@@ -0,0 +1,6 @@
+public class StringTrimExample{  
+public static void main(String args[]){  
+String s1="  hello string   ";  
+System.out.println(s1+"javaprogramming");//without trim()  
+System.out.println(s1.trim()+" javaprogramming");//with trim()  
+}} 
