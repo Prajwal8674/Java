cef5582b681a6d82e9f7a4d9ad77000808f830d3
intellij <patilprajwal8804@gmail.com>
2024-05-28 01:15:02 +0530

    commit

---
+++ core java/Day 5/StringProgram/StringClass.java
@@ -0,0 +1,9 @@
+class StringClass{
+public static void main(String args[]){
+String s1 = "hello";
+String s2 = new String("hello");
+if(s1.equals(s2)){
+System.out.println("true");
+}else{System.out.println("false");}
+}
+}
