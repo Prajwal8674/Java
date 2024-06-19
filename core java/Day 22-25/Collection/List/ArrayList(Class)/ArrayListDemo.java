ff61ccfdc5971951e2bbe6623b0c4fd5353a3e83
intellij <patilprajwal8804@gmail.com>
2024-06-20 00:25:54 +0530

    commit

---
+++ core java/Day 22-25/Collection/List/ArrayList(Class)/ArrayListDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class ArrayListDemo{
+	public static void main(String args[]){
+		ArrayList a = new ArrayList();
+		a.add("A");
+		a.add("10");
+		a.add("A");
+		a.add(null);
+		System.out.println(a);
+		a.remove(2);
+		System.out.println(a);
+		a.add(2,"M");
+		a.add("N");
+		System.out.println(a);
+	}
+}
