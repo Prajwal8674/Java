60068a2e1bf5229025d1e5d235fde0eb7c8e876d
intellij <patilprajwal8804@gmail.com>
2024-07-04 00:43:43 +0530

    commit

---
+++ core java/Day 16/Package/DateImpl.java
@@ -0,0 +1,11 @@
+package mypack;
+import mypack.MyDate;
+import java.util.*;
+
+public class DateImpl implements MyDate{
+	public void showDate(){
+	Date date = new Date();
+	System.out.println(date);
+	
+}
+}
