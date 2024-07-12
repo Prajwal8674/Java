7dc5a32a9acf4897742a084cdbd8b9f0125b0441
intellij <patilprajwal8804@gmail.com>
2024-07-13 00:34:01 +0530

    commit

---
+++ core java/Day 10/Static/Calculate.java
@@ -0,0 +1,12 @@
+//Program to get cube of a given number by static method  
+  
+class Calculate{  
+  static int cube(int x){  
+  return x*x*x;  
+  }  
+  
+  public static void main(String args[]){  
+  int result=Calculate.cube(5);  
+  System.out.println(result);  
+  }  
+}
